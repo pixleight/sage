5ba74470c75e8f747e7ca0ae3ff29ec9436de35b
Chris Violette <cvioletterun@gmail.com>
2016-04-22 15:38:09 -0400

    Updated base template and header to use mdl

--- base.php
+++ base.php
@@ -6,7 +6,7 @@ use Roots\Sage\Wrapper;
 ?>
 
 <!doctype html>
-<html <?php language_attributes(); ?>>
+<html class="no-js" <?php language_attributes(); ?>>
   <?php get_template_part('templates/head'); ?>
   <body <?php body_class(); ?>>
     <!--[if IE]>
@@ -14,26 +14,37 @@ use Roots\Sage\Wrapper;
         <?php _e('You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.', 'sage'); ?>
       </div>
     <![endif]-->
-    <?php
-      do_action('get_header');
-      get_template_part('templates/header');
-    ?>
-    <div class="wrap container" role="document">
-      <div class="content row">
-        <main class="main">
-          <?php include Wrapper\template_path(); ?>
-        </main><!-- /.main -->
-        <?php if (Setup\display_sidebar()) : ?>
-          <aside class="sidebar">
-            <?php include Wrapper\sidebar_path(); ?>
-          </aside><!-- /.sidebar -->
-        <?php endif; ?>
-      </div><!-- /.content -->
-    </div><!-- /.wrap -->
-    <?php
-      do_action('get_footer');
-      get_template_part('templates/footer');
-      wp_footer();
-    ?>
+    <div class="mdl-layout__container">
+      <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+        <?php
+          do_action('get_header');
+          get_template_part('templates/header');
+        ?>
+        <div class="mdl-layout__content" role="document">
+          <div class="content mdl-grid">
+            <?php
+            if( Setup\display_sidebar() ) :
+              $col = 'mdl-cell--8-col';
+            else :
+              $col = 'mdl-cell--12-col';
+            endif;
+            ?>
+            <main class="main mdl-cell <?php echo $col; ?>" role="main">
+              <?php include Wrapper\template_path(); ?>
+            </main><!-- /.main -->
+            <?php if (Setup\display_sidebar()) : ?>
+              <aside class="sidebar mdl-cell mdl-cell--4-col" role="complementary">
+                <?php include Wrapper\sidebar_path(); ?>
+              </aside><!-- /.sidebar -->
+            <?php endif; ?>
+          </div><!-- /.content.mdl-grid -->
+          <?php
+            do_action('get_footer');
+            get_template_part('templates/footer');
+            wp_footer();
+          ?>
+        </div><!-- /.md-layout__content -->
+      </div><!-- /.mdl-layout -->
+    </div><!-- /.mdl-layout__container -->
   </body>
 </html>
